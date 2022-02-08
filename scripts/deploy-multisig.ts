13fcd6e30ebedeb75ce634e59c92feb36560932b
Jacek Chmielewski <jchmielewski@teonite.com>
2022-02-08 13:37:57 +0100

    feat(multisig): proxy deployment & ownership transfer implementation

---
+++ scripts/deploy-multisig.ts
@@ -0,0 +1,17 @@
+import { ethers, upgrades } from "hardhat";
+
+
+async function main() {
+  const Greeter = await ethers.getContractFactory('Greeter');
+  console.log('Deploying Greeter...');
+  // const greeter = await upgrades.deployProxy(Greeter, [ethers.utils.formatBytes32String("Hello, multisig!"), { initializer: 'memory' }]);
+  const greeter = await upgrades.deployProxy(Greeter, ["Hello, multisig!"]);
+  console.log('Greeter deployed to:', greeter.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
