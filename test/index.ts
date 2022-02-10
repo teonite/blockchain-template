b468aef2d60a104067755e42eb147020227524dd
Jacek Chmielewski <jchmielewski@teonite.com>
2022-02-10 10:20:34 +0100

    feat(mutlisig): tests for GreeterV2

--- test/index.ts
+++ test/index.ts
@@ -4,8 +4,11 @@ import { ethers } from "hardhat";
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!" as string);
+    const greeter = await Greeter.deploy();
     await greeter.deployed();
+
+    const initTx = await greeter.initialize("Hello, world!");
+    await initTx.wait();
 
     expect(await greeter.greet()).to.equal("Hello, world!");
 
@@ -15,5 +18,36 @@ describe("Greeter", function () {
     await setGreetingTx.wait();
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
+
+describe("GreeterV2", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("GreeterV2");
+    const greeter = await Greeter.deploy();
+    await greeter.deployed();
+
+    const initTx = await greeter.initialize("Hello, world!");
+    await initTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+
+  it("Should foobar", async function () {
+    const Greeter = await ethers.getContractFactory("GreeterV2");
+    const greeter = await Greeter.deploy();
+    await greeter.deployed();
+
+    const initTx = await greeter.initialize("Hello, world!");
+    await initTx.wait();
+
+    expect(await greeter.foo()).to.equal("bar");
   });
 });
