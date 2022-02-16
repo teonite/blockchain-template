bd5eff8773e474fa48af78fe164fa3f9af853c46
Jacek Chmielewski <jchmielewski@teonite.com>
2022-02-16 09:32:56 +0100

    feat(polygon): add mumbai testnet configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -40,6 +40,11 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    "polygon-mumbai": {
+      url: process.env.POLYGON_MUMBAI_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
